cd7ebd20061b169939e0bbaf2ef68e592d186cc4
amansingh4u98 <amansingh199835@gmail.com>
2023-10-25 22:43:38 +0530

    springboot api project

---
+++ src/main/java/springboot/starter/project/topic/TopicRepository.java
@@ -0,0 +1,7 @@
+package springboot.starter.project.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String> {
+
+}
